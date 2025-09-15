e8c29e7905e631876246d2eca71dcad6b6ccbeae
Vdtry-06 <tunhoipro0306@gmail.com>
2025-09-15 10:14:13 +0700

    add rmi for disperse

---
+++ Network_Programming/rmi/Calculator.java
@@ -0,0 +1,8 @@
+package rmi;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Calculator extends Remote {
+    public int add(int a, int b) throws RemoteException;
+}
---
+++ Network_Programming/rmi/CalculatorIml.java
@@ -0,0 +1,18 @@
+package rmi;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class CalculatorIml extends UnicastRemoteObject implements Calculator {
+
+    // create constructor
+    public CalculatorIml() throws RemoteException {
+        super();
+    }
+
+    @Override
+    public int add(int a, int b) throws RemoteException {
+        System.out.println("Add request: (a=" + a + ", b=" + b + ")");
+        return a + b;
+    }
+}
